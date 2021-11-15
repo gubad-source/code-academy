7eb12cf4d053b13e5abc65a4b31005a811ef0c22
gubad-source <qubadmustafayev@gmail.com>
2021-11-15 14:32:27 +0400

    testimonial

--- edstudy/assets/js/main.js
+++ edstudy/assets/js/main.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
 
+    if ($('.testimonial-slider').length) {
+        $('.testimonial-slider').owlCarousel({
+            loop: true,
+            margin: 30,
+            nav: false,
+            center: true,
+            autoplay: true,
+            autoplayTimeout: 4000,
+            autoplayHoverPause: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                }
+            }
+        });
+    }
 
     let coursesTab=Array.from(document.querySelectorAll('.nav .nav-item'));
     coursesTab.forEach(elem=>{
