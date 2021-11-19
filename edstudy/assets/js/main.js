deae656d169e73dd08b24df00ee9a997abbdb3ba
gubad-source <qubadmustafayev@gmail.com>
2021-11-19 16:49:00 +0400

    newletter have been added to aside

--- edstudy/assets/js/main.js
+++ edstudy/assets/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
     $("#treeview .open").click(function (e) {
+        e.preventDefault();
         e.stopPropagation();
         $(this).children().eq(1).slideToggle();
       });
