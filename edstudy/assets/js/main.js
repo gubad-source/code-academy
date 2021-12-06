3fa410b3b4e0ccbeeb9440d50fcbd33aecece899
gubad-source <qubadmustafayev@gmail.com>
2021-12-06 11:30:11 +0400

    basket have been added kind a

--- edstudy/assets/js/main.js
+++ edstudy/assets/js/main.js
@@ -94,7 +94,119 @@ $(document).ready(function(){
         });
     }
 
+   
+
+var addToBasketBtn = document.querySelectorAll(".add-to-cart");
+var cartCount = document.querySelector(".fas.fa-shopping-basket");
+var cartItems = document.querySelector(".cart-items");
 
 
-      new WOW().init();
+
+addToBasketBtn.forEach(elem => {
+    elem.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        let basket = JSON.parse(localStorage.getItem("basket"));
+
+        if (basket == null) {
+            basket = {
+                items: [],
+                count: 0,
+                total: 0
+            };
+        }
+
+        let prudoctId = this.dataset.id;
+
+        let index = basket.items.findIndex((item) => {
+            return item.id == prudoctId;
+        });
+
+        if (index == -1) {
+            let product = {
+                id: this.dataset.id,
+                price: this.parentNode.parentNode.nextElementSibling.children[1].innerText,
+                image: this.parentNode.previousElementSibling.getAttribute('src'),
+                qty: 1
+            };
+
+            basket.items.push(product);
+        } else {
+            basket.items[index].qty++;
+        }
+
+        basket = calcTotalAndCount(basket);
+
+        if (cartCount != null)
+          
+            cartCount.nextElementSibling.innerText=basket.count;
+
+
+        localStorage.setItem("basket", JSON.stringify(basket));
+    });
+});
+
+let basket = JSON.parse(localStorage.getItem("basket"));
+
+if (basket != null && cartCount != null) {
+  
+
+    cartCount.nextElementSibling.innerText=basket.count;
+}
+
+if(basket!=null && cartItems!=null){
+
+    basket.items.forEach((item,index)=>{
+        let td = `<td class="col-sm-8 col-md-6">
+                    <div class="media">
+                        <a class="thumbnail pull-left" href="#"> 
+                            <img class="media-object" src="${item.image}" style="width: 72px; height: 72px;">
+                        </a>
+                      
+                    </div></td>
+                    <td class="col-sm-1 col-md-1" style="text-align: center">
+                    <input type="email" class="form-control" id="exampleInputEmail1" value="${item.qty}">
+                    </td>
+                    <td class="col-sm-1 col-md-1 text-center"><strong>${item.price} ₼</strong></td>
+                    <td class="col-sm-1 col-md-1 text-center"><strong>${item.price*item.qty} ₼</strong></td>
+                    <td class="col-sm-1 col-md-1">
+                    <button type="button" class="btn btn-danger remote-from-basket" data-id="${item.id}">
+                        <span class="glyphicon glyphicon-remove"></span> Remove
+                    </button>
+                </td>`;
+        
+        let tr = document.createElement('tr');
+        tr.dataset.index = index;
+        tr.innerHTML = td;
+
+        cartItems.prepend(tr);
+    });
+}
+
+
+var removeBtns = document.querySelectorAll(".remote-from-basket");
+
+removeBtns.forEach(item=>{
+    item.addEventListener("click",function(){
+        let index = item.parentNode.parentNode.dataset.index;
+        basket.items.splice(index,1);
+        item.parentNode.parentNode.remove();
+        
+        basket = calcTotalAndCount(basket);
+        localStorage.setItem("basket", JSON.stringify(basket));
+    });
+})
+
+function calcTotalAndCount(basket) {
+    basket.count = 0;
+    basket.total = 0;
+    basket.items.forEach(item => {
+        basket.total += (item.qty * item.price);
+        basket.count++;
+    });
+
+    return basket;
+}
+
+   new WOW().init();
 })
