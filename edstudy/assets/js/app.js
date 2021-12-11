7ac7eacb59bfabff8945f9be7de0eab84629bf01
gubad-source <qubadmustafayev@gmail.com>
2021-12-11 14:20:39 +0400

    sidebar and nav kind a

--- edstudy/assets/js/app.js
+++ edstudy/assets/js/app.js
@@ -11,22 +11,31 @@ $(window).on("load",function(){
   closeSidebar.addEventListener('click',function(){
     sidebarUnderlay.style.width='0px'
     sidebar.style.width='0px'
+  
   });
   
   hamburger.addEventListener('click',function(){
    
-   
       sidebarUnderlay.style.width='100%'
       sidebar.style.width='79%'
-     
+    closeSidebar.classList.toggle('active')
     })
+
+    document.querySelectorAll('ul .open').forEach(elem=>{
+      elem.addEventListener('click',(function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+ 
+      }))
+    })
+  
   
     $("ul .open").click(function (e) {
       e.preventDefault();
       e.stopPropagation();
       $(this).children().eq(1).slideToggle();
     });
-   
+
     let rotater=document.querySelectorAll('.rotating ul li');
     rotater.forEach((elem, index) => {
       if (elem.style.animation) {
